8ac20d8c12a785a51d33e728f1800689a87e4946
lam.lhd <lam.lhd@mor.com.vn>
2023-04-10 14:11:46 +0700

    study nestjs

---
+++ src/post/controllers/post.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreatePostDto, UpdatePostDto } from '../dto/post.dto';
+import { PostService } from '../services/post.service';
+import { Types } from 'mongoose';
+
+@Controller('post')
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Get()
+  getAllPost() {
+    return this.postService.getAllPosts();
+  }
+
+  @Get(':id')
+  getPostById(@Param('id') id: Types.ObjectId) {
+    return this.postService.getPostById(id);
+  }
+
+  @Post()
+  async createPost(@Body() post: CreatePostDto) {
+    return this.postService.createPost(post);
+  }
+
+  @Put(':id')
+  async replacePost(@Param('id') id: Types.ObjectId, @Body() post: UpdatePostDto) {
+    return this.postService.replacePost(id, post);
+  }
+
+  @Delete(':id')
+  async deletePost(@Param('id') id: Types.ObjectId) {
+    await this.postService.deletePost(id);
+    return true;
+  }
+}
